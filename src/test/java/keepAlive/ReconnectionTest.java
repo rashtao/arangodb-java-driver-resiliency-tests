0844c83e22ef7e79b987d9fadad840d981b8af82
Michele Rastelli <michele@arangodb.com>
2021-08-16 14:17:21 +0200

    reconnection failure test

--- src/test/java/keepAlive/ReconnectionTest.java
+++ src/test/java/keepAlive/ReconnectionTest.java
@@ -1,13 +1,19 @@
 package keepAlive;
 
+import ch.qos.logback.classic.Level;
 import com.arangodb.ArangoDB;
+import com.arangodb.ArangoDBException;
 import eu.rekawek.toxiproxy.Proxy;
 import eu.rekawek.toxiproxy.ToxiproxyClient;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import utils.MemoryAppender;
 
 import java.io.IOException;
+
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.assertj.core.api.Assertions.catchThrowable;
 
 /**
  * Start toxiproxy server before running
@@ -19,9 +25,11 @@ class ReconnectionTest {
     private Proxy proxy;
     private ToxiproxyClient client;
     private ArangoDB arangoDB;
+    private MemoryAppender memoryAppender;
 
     @BeforeEach
     void init() throws IOException {
+        memoryAppender = new MemoryAppender(Level.WARN);
         client = new ToxiproxyClient("127.0.0.1", 8474);
         proxy = client.createProxy("arango", "127.0.0.1:8529", "172.28.3.1:8529");
         arangoDB = new ArangoDB.Builder()
@@ -44,10 +52,32 @@ class ReconnectionTest {
     void closeAndReopenConnection() throws IOException {
         arangoDB.getVersion();
 
-        // delete and recreate proxy, closes the driver connection
-        proxy.delete();
-        proxy = client.createProxy("arango", "127.0.0.1:8529", "172.28.3.1:8529");
+        // closes the driver connection
+        proxy.disable();
+        proxy.enable();
 
         arangoDB.getVersion();
     }
+
+    /**
+     * log warnings produced by reconnection failure
+     */
+    @Test
+    void closeConnection() throws IOException {
+        arangoDB.getVersion();
+
+        // closes the driver connection
+        proxy.disable();
+
+        Throwable thrown = catchThrowable(() -> arangoDB.getVersion());
+        assertThat(thrown).isInstanceOf(ArangoDBException.class);
+        assertThat(thrown.getMessage()).contains("Cannot contact any host");
+
+        long warnsCount = memoryAppender.getLoggedEvents().stream()
+                .filter(e -> e.getMessage().contains("Could not connect to host[addr=127.0.0.1,port=8529]") &&
+                        e.getLevel().equals(Level.WARN))
+                .count();
+        assertThat(warnsCount >= 3);
+    }
+
 }
