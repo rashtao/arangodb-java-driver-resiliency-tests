2be310b71264d31487c6e314a186cdb87085c330
Michele Rastelli <michele@arangodb.com>
2021-08-18 20:41:39 +0200

    merged reconnection tests together

--- src/test/java/com/arangodb/resiliency/reconnection/ReconnectionTest.java
+++ src/test/java/com/arangodb/resiliency/reconnection/ReconnectionTest.java
@@ -27,54 +27,36 @@ class ReconnectionTest extends SingleServerTest {
     }
 
     /**
-     * if the proxy is disabled:
-     * - the subsequent requests should throw ArangoDBException("Cannot contact any host")
+     * on reconnection failure:
+     * - 3x logs WARN Could not connect to host[addr=127.0.0.1,port=8529]
+     * - ArangoDBException("Cannot contact any host")
      * <p>
      * once the proxy is re-enabled:
      * - the subsequent requests should be successful
      */
     @ParameterizedTest
     @MethodSource("arangoProvider")
-    void closeAndReopenConnection(ArangoDB arangoDB) throws IOException, InterruptedException {
-        arangoDB.getVersion();
-
-        // closes the driver connection
-        getProxy().disable();
-        Thread.sleep(100);
-
-        Throwable thrown = catchThrowable(arangoDB::getVersion);
-        assertThat(thrown).isInstanceOf(ArangoDBException.class);
-        assertThat(thrown.getMessage()).contains("Cannot contact any host");
-
-        getProxy().enable();
-        Thread.sleep(100);
-
-        arangoDB.getVersion();
-        arangoDB.shutdown();
-    }
-
-    /**
-     * on reconnection failure:
-     * - 3x logs WARN Could not connect to host[addr=127.0.0.1,port=8529]
-     * - ArangoDBException("Cannot contact any host")
-     */
-    @ParameterizedTest
-    @MethodSource("arangoProvider")
-    void closeConnection(ArangoDB arangoDB) throws IOException {
+    void closeAndReconnect(ArangoDB arangoDB) throws IOException, InterruptedException {
         arangoDB.getVersion();
 
         // close the driver connection
         getProxy().disable();
+        Thread.sleep(100);
 
         Throwable thrown = catchThrowable(arangoDB::getVersion);
         assertThat(thrown).isInstanceOf(ArangoDBException.class);
         assertThat(thrown.getMessage()).contains("Cannot contact any host");
 
         long warnsCount = memoryAppender.getLoggedEvents().stream()
-                .filter(e -> e.getMessage().contains("Could not connect to host[addr=127.0.0.1,port=8529]") &&
-                        e.getLevel().equals(Level.WARN))
+                .filter(e -> e.getLevel().equals(Level.WARN))
+                .filter(e -> e.getMessage().contains("Could not connect to host[addr=127.0.0.1,port=8529]"))
                 .count();
         assertThat(warnsCount).isGreaterThanOrEqualTo(3);
+
+        getProxy().enable();
+        Thread.sleep(100);
+
+        arangoDB.getVersion();
         arangoDB.shutdown();
     }
 
